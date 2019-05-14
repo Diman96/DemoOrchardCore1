2a5aa197f76b6be96f1ede8ee2a3d30577812bfe
Diman96 <nde96m@gmail.com>
2019-05-14 14:48:12 +0300

    Добавьте файлы проекта.

---
+++ DemoTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "DemoTheme",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    Description = "DemoTheme"
+)]
